c722acc827da24edaa7b87f984a76ec1cddef13b
abhibth977 <abhibth977@gmail.com>
2024-09-28 23:44:17 +0530

    updatig comments

--- middlewares/loggedIn.middleware.js
+++ middlewares/loggedIn.middleware.js
@@ -1,10 +1,16 @@
 const { tokenHelper } = require("../helper");
 
 module.exports = {
+    // Middleware function to check if the user is authenticated and logged in
     loggedIn: async (req, res, next) => {
+        // Decode the token from cookies using tokenHelper and pass the result to cookies variable    
         const cookies = await tokenHelper.decode(req.cookies['token'], next);
+
+        // Extract userId and role from the decoded token (if available)
         const userId = cookies?.userId;
         const role = cookies?.role;
+
+        // If no userId is found, set loggedIn to false (user is not authenticated)
         if (!userId) {
             res.locals.loggedIn = false;
         } else {
